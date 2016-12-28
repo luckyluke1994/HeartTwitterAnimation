930e819360c48fc46efb82b342d4f046bbd6d74b
luckyluke1994 <mddien1994@gmail.com>
2016-12-28 09:35:47 +0700

    first commit

---
+++ app/src/main/java/com/example/framgiamaidaidien/hearttwitteranimation/Utils.java
@@ -0,0 +1,15 @@
+package com.example.framgiamaidaidien.hearttwitteranimation;
+
+/**
+ * Created by FRAMGIA\mai.dai.dien on 28/12/2016.
+ */
+
+public class Utils {
+    public static double mapValueFromRangeToRange(double value, double fromLow, double fromHigh, double toLow, double toHigh) {
+        return toLow + ((value - fromLow) / (fromHigh - fromLow) * (toHigh - toLow));
+    }
+
+    public static double clamp(double value, double low, double high) {
+        return Math.min(Math.max(value, low), high);
+    }
+}
